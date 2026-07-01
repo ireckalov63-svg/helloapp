920a7a4360f7054fedc6fcca36669ff386c9f6a9
black <black@example.com>
2026-07-02 00:47:58 +0500

    Fix code: call greetWhite and greetBlack functions

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-function greetWhite() {
-    console.log("hello white"); 
-}
-
 console.log("hello everyone");
 
+greetWhite();
+greetBlack();
+
+function greetWhite() {
+    console.log("hello white");
+}
 
 function greetBlack() {
     console.log("hello black");
