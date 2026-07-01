792535cb254ad886c09dfe88f52d22cca1a2675b
black <black@example.com>
2026-07-02 00:38:17 +0500

    Add greetBlack function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-console.log("hello");
-
-// Новая функция (просто объявляем её, но не вызываем ниже!)
 function greetWhite() {
-    console.log("hello white");
+    console.log("hello white"); 
 }
+
 console.log("hello everyone");
+
+
+function greetBlack() {
+    console.log("hello black");
+}
